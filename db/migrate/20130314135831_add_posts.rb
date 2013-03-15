8e0083e2131ca9614d4414e209950581e2c11dd8
Matthew Fleming <flemingfx@gmaill.com>
2013-03-15 12:40:53 -0400

    initial commit

---
+++ db/migrate/20130314135831_add_posts.rb
@@ -0,0 +1,16 @@
+class AddPosts < ActiveRecord::Migration
+  def up
+    create_table :posts do |t|
+      t.string  :title
+      t.text  :body
+
+      t.timestamps
+
+    end
+
+  end
+
+  def down
+    drop_table :posts
+  end
+end
